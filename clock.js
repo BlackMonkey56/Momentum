5fcebc761ce068f2dd9e3a90d69f63c2ec796b06
BlackMonkey56 <blackmonkey9256@gmail.com>
2018-12-13 00:09:28 +0900

    Complete clock application

--- clock.js
+++ clock.js
@@ -1,6 +1,7 @@
 const clockContainer = document.querySelector(".js-clock"),
   clockTitle = clockContainer.querySelector("h1");
 
+//Date객체를 만들고 시간에 대한 데이터를 가져온다.
 function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
